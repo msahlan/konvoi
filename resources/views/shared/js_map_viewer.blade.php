763d0ab940de16fc0396efc73f12ef5ea4575573
Andy Awidarto <andy.awidarto@gmail.com>
2016-11-03 16:24:38 +0700

    map viewer

--- resources/views/shared/js_map_viewer.blade.php
+++ resources/views/shared/js_map_viewer.blade.php
@@ -3,7 +3,7 @@
                     var lat = target.data('lat');
                     var lon = target.data('lon');
 
-                    var gs_src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + lat +','+ lon + '&markers=color:red%7Clabel:P&zoom=12&size=400x400&key={{env('GOOGLE_MAP_KEY')}}';
+                    var gs_src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + lat +','+ lon + '&markers=color:red%7Clabel:P%7C' + lat +','+ lon + '&zoom=12&size=400x400&key={{env('GOOGLE_MAP_KEY')}}';
 
                     $('#map-view').attr('src', gs_src);
 
